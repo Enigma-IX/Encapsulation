32349b9a0713e2253762e28a1c70c9ad8a146942
Loris Tessier <loris.tessier34@orange.fr>
2024-11-18 10:10:56 +0100

    Added framework and fixed SDL-Raylib switch

--- Encapsulation/main.cpp
+++ Encapsulation/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include "framework.h"
+
 #include "SDLWindow.h"
 #include "RaylibWindow.h"
 
-#define USE_SDL = true;
 
-#ifdef USE_SDL
+#define USE_SDL false
+
+#if USE_SDL
 using CurrentWindow = SDLWindow;
 #else
 using CurrentWindow = RaylibWindow;
 #endif
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     CurrentWindow window;
-    if (!window.initialize() || !window.createWindow(800, 600, "Encapsulation")) {
+    if (!window.initialize() || !window.createWindow(WIN_WIDTH, WIN_HEIGHT, "Encapsulation")) 
+    {
         std::cerr << "Failed to initialize window!" << std::endl;
         return -1;
     }
@@ -20,9 +25,12 @@ int main(int argc, char* argv[]) {
     float x = 400, y = 300, dx = 2.5f, dy = 2.0f, radius = 20; // TO DO: Gerer ca avec dans les classes sprite
 
     SDL_Event event;
-    while (window.isOpen()) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+    while (window.isOpen())
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT) 
+            {
                 window.close();
                 break;
             }
@@ -32,8 +40,8 @@ int main(int argc, char* argv[]) {
         // TO DO: Gerer ca avec dans les classes sprite  (Logique de rebonsissement)
         x += dx;
         y += dy;
-        if (x - radius < 0 || x + radius > 800) dx = -dx;
-        if (y - radius < 0 || y + radius > 600) dy = -dy;
+        if (x - radius < 0 || x + radius > WIN_WIDTH) dx = -dx;
+        if (y - radius < 0 || y + radius > WIN_HEIGHT) dy = -dy;
 
         window.drawCircle(x, y, radius);
         window.display();

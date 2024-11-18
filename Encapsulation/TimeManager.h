24c301a4a413fe8c6c704e5178b8a982389205a9
Loris Tessier <loris.tessier34@orange.fr>
2024-11-18 12:59:35 +0100

    TimeManger Done

--- Encapsulation/TimeManager.h
+++ Encapsulation/TimeManager.h
@@ -1,5 +1,42 @@
-#pragma once
-class TimeManager
-{
+#include <chrono>
+
+class TimeManager {
+public:
+	// Singleton instance access
+	static TimeManager& Instance()
+	{
+		static TimeManager instance;
+		return instance;
+	}
+
+	// Delete copy/move constructors
+	TimeManager(const TimeManager&) = delete;
+	TimeManager& operator=(const TimeManager&) = delete;
+
+	// Update delta time
+	void Update()
+	{
+		auto currentTimePoint = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> elapsedTime = currentTimePoint - lastTimePoint;
+		deltaTime = elapsedTime.count();
+		lastTimePoint = currentTimePoint;
+	}
+
+	// Get delta time
+	double GetDeltaTime() const
+	{
+		return deltaTime;
+	}
+
+private:
+	// Private constructor for Singleton
+	TimeManager()
+	{
+		lastTimePoint = std::chrono::high_resolution_clock::now();
+		deltaTime = 0.0;
+	}
+
+	std::chrono::high_resolution_clock::time_point lastTimePoint;
+	double deltaTime;
 };
 

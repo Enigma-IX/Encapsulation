d482ea88acc98b3e70397ce5d0281d4fb8c679c9
Liza Harcheb <liza.harcheb.pro@gmail.com>
2024-11-18 14:51:36 +0100

    feat: Creation de la page SDL Sprite

--- Encapsulation/SDLSprite.cpp
+++ Encapsulation/SDLSprite.cpp
@@ -1,1 +1,39 @@
 #include "SDLSprite.h"
+#include <SDL_image.h>
+
+SDLSprite::SDLSprite(SDL_Renderer* renderer) : texture(nullptr), renderer(renderer), positionX(0), positionY(0) {};
+
+SDLSprite::~SDLSprite() 
+{
+	if (texture) 
+	{
+		SDL_DestroyTexture(texture);
+	}
+}
+
+bool SDLSprite::LoadImage(const std::string& filepath)
+{
+	SDL_Surface* surface = IMG_Load(filepath.c_str());
+	if (surface == false) {
+		return false;
+	}
+
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	return texture != nullptr;
+}
+
+void* SDLSprite::GetData() const 
+{
+	return texture;
+}
+
+void SDLSprite::Draw(float x, float y)
+{
+	if (texture == false) {
+		return;
+	}
+
+	
+}
--- Encapsulation/SDLSprite.h
+++ Encapsulation/SDLSprite.h
@@ -1,12 +1,25 @@
 #pragma once
 #include "Sprite.h"
+#include <SDL.h>
+#include <string>
+
 class SDLSprite : public Sprite
 {
+
 public:
-	SDLSprite();
+	SDLSprite(SDL_Renderer* renderer);
 	~SDLSprite();
 
-	void loadSprite() override;
-	void getLoadedSprite() override;
+	bool LoadImage(const std::string& filepath) override;
+	void* GetData() const override;
+	void Draw(float x, float y) override;
+	void SetPosition(float x, float y) override;
+	std::pair<float, float> GetPosition() const override;
+
+
+private:
+	SDL_Texture* texture;
+	SDL_Renderer* renderer;
+	float positionX, positionY;
 };
 

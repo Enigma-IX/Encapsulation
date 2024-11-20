da79717be986d2116a09c8d154a555ba2e6eb268
Liza Harcheb <liza.harcheb.pro@gmail.com>
2024-11-20 11:01:49 +0100

    feat: Ajout du fps Texte et init de variables

--- Encapsulation/FPSCounter.cpp
+++ Encapsulation/FPSCounter.cpp
@@ -1,19 +1,31 @@
 #include "FPSCounter.h"
 
-FPSCounter::FPSCounter(Text* texte)
+FPSCounter::FPSCounter(Text* texte) : frameCount(0), elapsedTime(0.0), FPS(0.0)
 {
 	fpsText = texte;
 }
 
 FPSCounter::~FPSCounter()
 {
-
+	delete fpsText;
 }
 
 void FPSCounter::Update(double deltaTime)
 {
+	elapsedTime += deltaTime;
+	frameCount++;
+
+	if (elapsedTime >= 1)
+	{
+		FPS = frameCount / elapsedTime;
+		frameCount = 0;
+		elapsedTime = 0;
+
+		std::string fpsString = "FPS : " + std::to_string(static_cast<int>(FPS));
+	}
 }
 
 void FPSCounter::Renderer()
 {
+	fpsText->render();
 }
